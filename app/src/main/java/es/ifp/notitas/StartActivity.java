dde51ad78bfea5ba1c3cba42b227205e1d4f7427
Diegantos <diegantos8@gmail.com>
2023-02-27 11:43:29 +0100

    new imports

---
+++ app/src/main/java/es/ifp/notitas/StartActivity.java
@@ -0,0 +1,40 @@
+package es.ifp.notitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StartActivity extends AppCompatActivity {
+
+    protected TextView label1;
+    protected ImageView img1;
+    private Intent pasarPantalla;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        label1 = (TextView) findViewById(R.id.label1_start);
+        img1 = (ImageView) findViewById(R.id.img1_start);
+
+        TimerTask myTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                pasarPantalla = new Intent(StartActivity.this, StartActivity.this);
+                finish();
+                startActivity(pasarPantalla);
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(myTimerTask, 2000);
+
+    }
+}
