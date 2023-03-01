3613a6cbd1cbd32d7e82a97cbff0df40f37fc8ab
Diegantos <diegantos8@gmail.com>
2023-03-01 18:37:13 +0100

    Referenced and btn events

--- app/src/main/java/es/ifp/notitas/ShowNoteActivity.java
+++ app/src/main/java/es/ifp/notitas/ShowNoteActivity.java
@@ -3,12 +3,37 @@ package es.ifp.notitas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class ShowNoteActivity extends AppCompatActivity {
 
+    protected ListView list1;
+    protected Button btn1;
+    protected Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_note);
+        getSupportActionBar().hide();
+
+        list1 = (ListView) findViewById(R.id.list1_show);
+        btn1 = (Button) findViewById(R.id.btn1_show);
+        btn2 = (Button) findViewById(R.id.btn2_show);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
