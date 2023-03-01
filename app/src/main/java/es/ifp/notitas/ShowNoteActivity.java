fc2d9e7a9dae2a2e72fd9e072f8f39c81f5baafd
Diegantos <diegantos8@gmail.com>
2023-03-01 18:22:56 +0100

    Show Note Activity

---
+++ app/src/main/java/es/ifp/notitas/ShowNoteActivity.java
@@ -0,0 +1,14 @@
+package es.ifp.notitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_note);
+    }
+}
